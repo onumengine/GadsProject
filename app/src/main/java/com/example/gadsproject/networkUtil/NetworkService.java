97757ec0fa25625175cdecd1f57b674698fe27e3
onumengine <onumdev@gmail.com>
2020-09-07 10:32:28 +0100

    Added the ServiceBuilder class

--- app/src/main/java/com/example/gadsproject/networkUtil/NetworkService.java
+++ app/src/main/java/com/example/gadsproject/networkUtil/NetworkService.java
@@ -7,12 +7,13 @@ import java.util.List;
 
 import retrofit2.Call;
 import retrofit2.http.GET;
+import retrofit2.http.Path;
 
 public interface NetworkService
 {
-    @GET("/api/hours")
-    Call<ArrayList<UserInfo>> getTopLearners();
+    @GET("api/{hours}")
+    Call<ArrayList<UserInfo>> getTopLearners(@Path("hours")String hours);
 
-    @GET("/api/skilliq")
-    Call<ArrayList<UserInfo>> getTopScorers();
+    @GET("api/{skilliq}")
+    Call<ArrayList<UserInfo>> getTopScorers(@Path("skilliq")String skillIq);
 }
---
+++ app/src/main/java/com/example/gadsproject/networkUtil/ServiceBuilder.java
@@ -0,0 +1,24 @@
+package com.example.gadsproject.networkUtil;
+
+import okhttp3.OkHttpClient;
+import okhttp3.logging.HttpLoggingInterceptor;
+import retrofit2.Retrofit;
+import retrofit2.converter.gson.GsonConverterFactory;
+
+public class ServiceBuilder
+{
+    private static final String URL = "https://gadsapi.herokuapp.com";
+
+    private static HttpLoggingInterceptor loggingInterceptor = new HttpLoggingInterceptor().setLevel(HttpLoggingInterceptor.Level.BODY);
+
+    private static OkHttpClient.Builder okHttpCLient = new OkHttpClient.Builder().addInterceptor(loggingInterceptor);
+
+    private static Retrofit.Builder builder = new Retrofit.Builder().baseUrl(URL).addConverterFactory(GsonConverterFactory.create()).client(okHttpCLient.build());
+
+    private static Retrofit retrofit = builder.build();
+
+    public static <s> s buildService(Class<s> serviceInterface)
+    {
+        return retrofit.create(serviceInterface);
+    }
+}
